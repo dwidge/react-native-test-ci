6d7132227c3b12a0c2f15f4e47f4fa2463958ba3
dwidge <dwidge+gh@gmail.com>
2023-10-26 11:43:41 +0200

    Time

--- __tests__/Example.test.tsx
+++ __tests__/Example.test.tsx
@@ -11,8 +11,9 @@ test('examples of some things', async () => {
 
   fireEvent.changeText(screen.getByTestId('input'), expectedUsername);
   fireEvent.press(screen.getByText('Print Username'));
-  const usernameOutput = await waitFor(() =>
-    screen.getByTestId('printed-username'),
+  const usernameOutput = await waitFor(
+    () => screen.getByTestId('printed-username'),
+    {timeout: 1},
   );
   // expect(usernameOutput.props.children).toBe(expectedUsername);
   // expect(usernameOutput.props.children).not.toBe(expectedUsername + '123');
